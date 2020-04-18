7cfdd38eb52836be9567e9ca0b21b660b978126c
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-04-18 21:27:21 +0900

    AllFinishgit add .

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,6 +3,7 @@ def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
+    comment.user_id = current_user.id
     comment.save
     redirect_to book_path(book)
 end
